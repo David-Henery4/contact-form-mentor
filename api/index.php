ef1948fc3fe4085fa715ae4657a608d160cb06bc
DizzlyD <david4henery00@hotmail.com>
2025-03-27 14:21:25 +0000

    added-vercel-php

--- api/index.php
+++ api/index.php
@@ -3,11 +3,13 @@
 
 <?php include "./src/partials/head.php"; ?>
 
+
+
 <body class="bg-primaryGreen text-darkGrey font-karla not-italic py-8 px-4 smTablet:py-32 tablet:px-10">
 
   <div class="w-full max-w-3xl mx-auto rounded-2xl p-6 bg-white smTablet:p-10">
     <h1 class="text-3xl font-bold text-darkGrey font-karla">Contact Us</h1>
-    <?php include __DIR__ . "/src/partials/form.php"; ?>
+    <?php include __DIR__ . "../src/partials/form.php"; ?>
   </div>
 
 </body>
